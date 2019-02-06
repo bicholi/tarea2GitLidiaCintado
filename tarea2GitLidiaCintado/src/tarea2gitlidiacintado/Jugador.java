808b62653a46d20a21569928cd45cdd2c268bfb4
lidia <lidiacintadog@gmail.com>
2019-02-06 13:40:17 +0100

    Añade getters y setters

--- tarea2GitLidiaCintado/src/tarea2gitlidiacintado/Jugador.java
+++ tarea2GitLidiaCintado/src/tarea2gitlidiacintado/Jugador.java
@@ -10,13 +10,40 @@ package tarea2gitlidiacintado;
  * @author bicho
  */
 public class Jugador {
-    String nombre;     // Nombre del jugador de fútbol
-    int votos;         // Votos recibidos en la aplicación
-    int id;            // Número único identificativo
+    private String nombre;     // Nombre del jugador de fútbol
+    private int votos;         // Votos recibidos en la aplicación
+    private int id;            // Número único identificativo
 
     public Jugador(String nombre, int votos, int id) {
         this.nombre = nombre;
         this.votos = votos;
         this.id = id;
     }  
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+    
 }
